69a2e365225c1d0d25b14daaedf0661a5c1f81b1
adifa <adi.fayer@gmail.com>
2024-12-28 22:20:16 +0200

    commit

--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Create from "./Create.jsx";
 import { getTasks, handleUpdateTask, handleDeleteTask } from "./httpUtil.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
